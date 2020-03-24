3a556cb42e131525b6ead4e429ed29f82aa71c7b
Peter Abrahamsen <rainhead@gmail.com>
2020-03-24 14:07:45 -0700

    Move cbindgen config into toml file

--- build.rs
+++ build.rs
@@ -1,14 +1,11 @@
-extern crate cbindgen;
-
+use cbindgen;
 use std::env;
 
-fn main() {
+fn main() -> Result<(), cbindgen::Error> {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-      .with_crate(crate_dir)
-      .with_language(cbindgen::Language::C)
-      .generate()
-      .expect("Unable to generate bindings")
+    cbindgen::generate(crate_dir)?
       .write_to_file("bindings.h");
+
+    Ok(())
 }
