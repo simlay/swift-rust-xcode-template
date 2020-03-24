ac3c97b1c92499e4bc6b97a502277a070080d64b
Peter Abrahamsen <rainhead@gmail.com>
2020-03-24 14:15:23 -0700

    Use FfiStr to receive strings from Swift

--- bindings.h
+++ bindings.h
@@ -3,8 +3,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-char *rust_hello(const char *to);
+/**
+ * `FfiStr`, from the `ffi-support` crate, makes passing C-style (null-terminated) strings a little more convenient
+ * and a little safer. The `to` and `name` variables here cannot outlive the call to `rust_hello`.
+ */
+char *rust_hello(FfiStr to) NS_SWIFT_NAME(rust_hello(to:));
 
-void rust_hello_free(char *s);
-
-void use_swift_closure(int32_t (*swift_fn)(int32_t));
+void use_swift_closure(int32_t (*swift_fn)(int32_t)) NS_SWIFT_NAME(use_swift_closure(swift_fn:));
